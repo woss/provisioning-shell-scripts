2a88c2b221fdd72dc275b5744dc402ebe20b5291
Fredrik Wollsén <fredrik@neam.se>
2014-04-17 13:07:11 +0300

    Added en us locale to mosh provisioning

--- debian-wheezy/mosh.sh
+++ debian-wheezy/mosh.sh
@@ -1,4 +1,6 @@
 apt-get update
 apt-get install -y -q mosh
+# installed locales should match the locales of the users that use mosh
+locale-gen en_US.UTF-8
 locale-gen sv_SE.UTF-8
 echo "Now open the UDP ports 60000 and 61000 using Turnkey Linux Hub Servers Dashboard. Instance -> Firewall rules -> Add UDP 60000:61000"
